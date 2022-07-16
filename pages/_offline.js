5e603b2e9ef1ef88dca00cd122bea57ee322c0a6
Sahil <aggarwalsahil2004@gmail.com>
2022-07-16 21:45:34 +0530

    rebuild

--- pages/_offline.js
+++ pages/_offline.js
@@ -1,18 +1,15 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-// import React, { useEffect } from 'react'
-// import { useRouter } from 'next/router'
+import Head from "next/head";
 
 export default function Offline() {
-    // const router = useRouter();
-
-    // useEffect(() => { if (navigator.onLine) router.route('/') }, [])
-
-    // document.title = 'You are Offline'
-
-    return <div className="w-screen h-screen flex justify-center items-center inset-0 fixed z-20">
-        <div className='text-center px-4 font-sans space-y-2'>
-            <h1 className='text-3xl'>Offline...</h1>
-            <p>{"The current page isn't available offline. Please try again when you're back online."}</p>
+    return <>
+        <Head>
+            <title>You are Offline!</title>
+        </Head>
+        <div className="w-screen h-screen flex justify-center items-center inset-0 fixed z-20">
+            <div className='text-center px-4 font-sans space-y-2'>
+                <h1 className='text-3xl'>Offline...</h1>
+                <p>{"The current page isn't available offline. Please try again when you're back online."}</p>
+            </div>
         </div>
-    </div>
+    </>
 }
