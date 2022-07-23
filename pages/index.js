478c6e517af38681ae7c500abc313884b366492c
Sahil <aggarwalsahil2004@gmail.com>
2022-07-23 15:58:33 +0530

    rebuild

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     }
 
     return <div className='flex flex-col mb-12 space-y-8 md:flex-row md:space-y-0 items-center justify-evenly text-center md:h-[calc(100vh-8rem)]'>
-        <div className='shadow-md rounded-lg w-80 h-80 max-w-[80vw] cursor-pointer hover:shadow-2xl transition-all duration-300 relative' onPointerEnter={() => setHover(true)} onPointerLeave={() => setHover(false)} onClick={() => redirect('/file/upload')} onContextMenu={event => event.preventDefault()}>
+        <div className='shadow-md rounded-lg w-80 h-80 max-w-[80vw] cursor-pointer hover:shadow-2xl transition-all duration-300 relative' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={() => { setHover(true); redirect('/file/upload') }} onContextMenu={event => event.preventDefault()}>
             <Image src='/cloud.jpg' alt='Upload file' width={250} height={250} objectFit='scale-down' quality={100} />
             <div className={`absolute top-[10.25rem] md:top-44 left-1/2 -translate-x-1/2 transition-all duration-300 ${hover ? '-translate-y-[5.5rem] opacity-100' : 'opacity-0'}`}>
                 <Image src='/arrow.png' alt='Upload file' width={50} height={50} objectFit='scale-down' quality={100} />
