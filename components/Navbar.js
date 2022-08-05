ff70c108bdd8a5dc27d34dd68589dc6e44f80a8f
Sahil <aggarwalsahil2004@gmail.com>
2022-08-05 21:03:59 +0530

    better navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaUserAlt, FaUpload, FaDownload } from 'react-icons/fa'
+import { FaHome, FaUpload, FaDownload, FaHistory, FaUserAlt } from 'react-icons/fa'
 import LoadingBar from 'react-top-loading-bar'
 import { useFileContext } from '../contexts/ContextProvider'
 
 export default function Navbar() {
-    const { progress, setProgress } = useFileContext()
+    const { router, progress, setProgress } = useFileContext()
 
     return <>
         <LoadingBar color='#ffffff' progress={progress} waitingTime={300} onLoaderFinished={() => setProgress(0)} />
@@ -14,9 +14,14 @@ export default function Navbar() {
             {/* adding <a> tag inside <Link> as for some reason <Link> passes a ref to its child and we can't use ref in a react component(here, FaHistory) */}
             {/* title attribute displays text on element hover */}
             <div className='flex space-x-5 sm:space-x-6'>
-                <Link href="/account"><a><FaUserAlt className='scale-110 sm:scale-125' title='Your account' /></a></Link>
-                <Link href="/file/upload"><a><FaUpload className='scale-110 sm:scale-125 text-white' title='Upload File' /></a></Link>
-                <Link href="/file/download"><a><FaDownload className='scale-110 sm:scale-125 text-white' title='Download File' /></a></Link>
+                {router.pathname === '/' ?
+                    <Link href="/account/history?filter=upload"><a><FaHistory className='scale-110 sm:scale-125' title='History' /></a></Link> : <>
+                        <Link href="/"><a><FaHome className='hidden sm:block scale-125 sm:scale-150' title='Home' /></a></Link>
+                        <Link href="/account"><a><FaUserAlt className='scale-110 sm:scale-125' title='Your account' /></a></Link>
+                    </>
+                }
+                <Link href="/file/upload"><a><FaUpload className='scale-110 sm:scale-125' title='Upload File' /></a></Link>
+                <Link href="/file/download"><a><FaDownload className='scale-110 sm:scale-125' title='Download File' /></a></Link>
             </div>
         </nav >
         <div className='h-10' />
