95e8c0fe6e4e79691489e7b9487a62ae3304055d
Sahil <aggarwalsahil2004@gmail.com>
2022-08-13 13:27:19 +0530

    qr scan fix

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps }) {
     const router = useRouter()
     const [loading, setLoading] = useState(true)
     const hideNavbar = ['/_error', '/account/confirm/[token]', '/account/delete/[token]']
-    const showModal = ['/account', '/account/history', '/file']
+    const showModal = ['/account', '/account/history', '/file/download']
 
     useEffect(() => { setLoading(false) }, [])
 
