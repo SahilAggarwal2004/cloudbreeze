51d0f5f32ccd4b1bb9f5bb5b084e5640fd92a240
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2022-12-24 22:38:49 +0530

    commit

--- modules/functions.js
+++ modules/functions.js
@@ -20,42 +20,16 @@ export function generateId(value) {
 
 export function download(data, name, source) {
     try {
-        const streamSaver = require('streamsaver')
         if (source === 'local') var blob = data
         else {
             blob = new Blob([])
             data.forEach(chunk => { blob = new Blob([blob, chunk]) });
         }
-        const fileStream = streamSaver.createWriteStream(name, { size: blob.size })
-        const readableStream = blob.stream()
-        if (window.WritableStream && readableStream.pipeTo) return readableStream.pipeTo(fileStream).then(() => toast.success('File downloaded successfully!'))
-
-        // Write (pipe) manually
-        window.writer = fileStream.getWriter()
-
-        const reader = readableStream.getReader()
-        const pump = () => reader.read().then(res => {
-            if (res.done) {
-                writer.close()
-                toast.success('File downloaded successfully!')
-            } else writer.write(res.value).then(pump)
-        })
-        pump()
-    } catch { toast.error("Couldn't download file") }
+        const url = window.URL.createObjectURL(blob);
+        toast.success('File downloaded successfully!')
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = name; // giving default name to download prompt
+        a.click();
+    } catch { return toast.error("Couldn't download file") }
 }
-
-// export function download(data, name, source) {
-//     try {
-//         if (source === 'local') var blob = data
-//         else {
-//             blob = new Blob([])
-//             data.forEach(chunk => { blob = new Blob([blob, chunk]) });
-//         }
-//         const url = window.URL.createObjectURL(blob);
-//         toast.success('File downloaded successfully!')
-//         const a = document.createElement('a');
-//         a.href = url;
-//         a.download = name; // giving default name to download prompt
-//         a.click();
-//     } catch { return toast.error("Couldn't download file") }
-// }
