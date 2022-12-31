ba82a1305df3555e3d3dd5418f4da63076ae3012
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2022-12-31 08:24:28 +0530

    rebuild

--- constants.js
+++ constants.js
@@ -5,4 +5,4 @@ export const filePaths = ['/file/upload', '/file/download', '/p2p']
 export const options = ['upload', 'download']
 export const types = ['normal', 'premium']
 export const limit = 100 // In MB
-export const chunkSize = 65,536 // In bytes (64 KB)
+export const chunkSize = 65536 // In bytes (64 KB)
