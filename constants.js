b9b780553c90732fd7e557b99d511e0bb780030b
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2022-12-31 07:16:30 +0530

    Decreased chunk size (experimental)

--- constants.js
+++ constants.js
@@ -5,4 +5,4 @@ export const filePaths = ['/file/upload', '/file/download', '/p2p']
 export const options = ['upload', 'download']
 export const types = ['normal', 'premium']
 export const limit = 100 // In MB
-export const chunkSize = 1048576 // In bytes (1 MB)
+export const chunkSize = 65,536 // In bytes (64 KB)
