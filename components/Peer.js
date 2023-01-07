83363c0b88a0c686d772e23aaaea83e1090dd17d
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-01-07 17:16:57 +0530

    commit

--- components/Peer.js
+++ components/Peer.js
@@ -25,7 +25,7 @@ export default function Peer({ peer, names, sizes, totalSize, conn }) {
                 clearInterval(proceed)
                 if (i < files.length - 1) sendFile(i + 1)
             } else if (bytesSent - getStorage('bytes') < 20971520) conn.send({ file: file.slice(bytesSent, bytesSent += chunkSize), type: 'file' })
-        }, 20);
+        }, 10);
     }
 
     function acceptData({ type, bytesReceived = 0, totalBytesReceived = 0 }) {
