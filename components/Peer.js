405d96b01c7f5c6711c6e3159c3ead6a620c8d06
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-01-07 18:08:24 +0530

    bug fix

--- components/Peer.js
+++ components/Peer.js
@@ -14,19 +14,18 @@ export default function Peer({ peer, names, sizes, totalSize, conn }) {
     const [totalBytes, setTotalBytes] = useState(0)
     const [time, setTime] = useState(0)
     const size = sizes[count]
-    const getBytes = () => getStorage(`bytes-${peer}`)
+    const getBytes = () => getStorage(`bytes-${peer}`, 0)
     const setBytes = value => setStorage(`bytes-${peer}`, value)
 
     function sendFile(i = 0) {
         const file = files[i]
         const size = sizes[i]
+        setBytes(0)
         conn.send({ file: file.slice(0, chunkSize), name: names[i], size, type: 'file', initial: true })
         let bytesSent = chunkSize
         const proceed = setInterval(() => {
-            if (bytesSent >= size) {
-                clearInterval(proceed)
-                if (i < files.length - 1) sendFile(i + 1)
-            } else if (bytesSent - getBytes() < 20971520) conn.send({ file: file.slice(bytesSent, bytesSent += chunkSize), type: 'file' })
+            if (bytesSent >= size) clearInterval(proceed)
+            else if (bytesSent - getBytes() < 20971520) conn.send({ file: file.slice(bytesSent, bytesSent += chunkSize), type: 'file' })
         }, 20);
     }
 
@@ -50,6 +49,14 @@ export default function Peer({ peer, names, sizes, totalSize, conn }) {
             conn.close()
         }
     }, [])
+
+    useEffect(() => {
+        if (!count) return
+        conn.removeAllListeners('data')
+        if (totalBytes >= totalSize) return
+        conn.on('data', acceptData)
+        sendFile(count)
+    }, [count])
 
     return <div className='relative flex flex-col justify-center p-4 pb-0 border rounded text-center bg-gray-50 hover:bg-transparent hover:shadow-lg transition-all duration-300 min-w-[270px]'>
         <GoX className='absolute top-2 right-2 scale-110' onClick={() => conn.close()} />
