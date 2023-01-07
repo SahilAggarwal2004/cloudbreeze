c49e62b330c11716b1b9911644c8bb08b1efd3f0
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-01-07 17:42:02 +0530

    commit

--- components/Peer.js
+++ components/Peer.js
@@ -6,44 +6,31 @@ import { bytesToSize, speed } from '../modules/functions'
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import { useFileContext } from '../contexts/ContextProvider'
-import { getStorage, setStorage } from '../modules/storage'
 
 export default function Peer({ peer, names, sizes, totalSize, conn }) {
-    const { files } = useFileContext()
+    const { files } = useFileContext();
     const [count, setCount] = useState(0)
+    const [bytes, setBytes] = useState(0)
     const [totalBytes, setTotalBytes] = useState(0)
     const [time, setTime] = useState(0)
+    const file = files[count]
+    const name = names[count]
     const size = sizes[count]
-    const getBytes = () => getStorage(`bytes-${peer}`)
-    const setBytes = value => setStorage(`bytes-${peer}`, value)
-
-    function sendFile(i = 0) {
-        const file = files[i]
-        const size = sizes[i]
-        conn.send({ file: file.slice(0, chunkSize), name: names[i], size, type: 'file', initial: true })
-        let bytesSent = chunkSize
-        const proceed = setInterval(() => {
-            if (bytesSent >= size) {
-                clearInterval(proceed)
-                if (i < files.length - 1) sendFile(i + 1)
-            } else if (bytesSent - getBytes() < 20971520) conn.send({ file: file.slice(bytesSent, bytesSent += chunkSize), type: 'file' })
-        }, 25);
-    }
 
     function acceptData({ type, bytesReceived = 0, totalBytesReceived = 0 }) {
         if (type === 'request') {
             toast.success(`Transferring file(s) to ${peer}`)
             setTime(Date.now())
-            sendFile()
+            conn.send({ file: file.slice(0, chunkSize), name, size, type: 'file', initial: true })
         } else if (type === 'proceed') {
             setBytes(bytesReceived)
             setTotalBytes(totalBytesReceived)
-            if (bytesReceived >= size) setCount(count + 1)
+            if (bytesReceived < size) conn.send({ file: file.slice(bytesReceived, bytesReceived + chunkSize), type: 'file' })
+            else setCount(count + 1)
         }
     }
 
     useEffect(() => {
-        setBytes(0)
         conn.on('data', acceptData)
         return () => {
             conn.removeAllListeners()
@@ -51,10 +38,18 @@ export default function Peer({ peer, names, sizes, totalSize, conn }) {
         }
     }, [])
 
+    useEffect(() => {
+        if (!count) return
+        conn.removeAllListeners('data')
+        if (totalBytes >= totalSize) return
+        conn.on('data', acceptData)
+        conn.send({ file: file.slice(0, chunkSize), name, size, type: 'file', initial: true })
+    }, [count])
+
     return <div className='relative flex flex-col justify-center p-4 pb-0 border rounded text-center bg-gray-50 hover:bg-transparent hover:shadow-lg transition-all duration-300 min-w-[270px]'>
         <GoX className='absolute top-2 right-2 scale-110' onClick={() => conn.close()} />
         <h4 className='font-medium'>{peer}</h4>
-        <CircularProgressbarWithChildren value={getBytes()} maxValue={size} strokeWidth={2.5} className='scale-75' styles={{ path: { stroke: '#48BB6A' } }}>
+        <CircularProgressbarWithChildren value={bytes} maxValue={size} strokeWidth={2.5} className='scale-75' styles={{ path: { stroke: '#48BB6A' } }}>
             <div className='text-sm md:text-base text-center space-y-1 w-1/2 break-words'>
                 <div>{bytesToSize(totalBytes)} / {bytesToSize(totalSize, true)}</div>
                 <div>{count} / {names.length} files transferred</div>
