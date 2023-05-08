fe5c9647b53d1722f54cf2351a95ee50561af6cc
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-05-08 15:03:25 +0530

    commit

--- pages/file/upload.js
+++ pages/file/upload.js
@@ -12,125 +12,130 @@ import Head from 'next/head';
 import { randomElement } from 'random-stuff-js';
 
 export default function Upload({ router }) {
-  const { fetchApp, files, setFiles, uploadFiles, setUploadFiles, type } = useFileContext()
-  const { share } = router.query
-  const fileIdRef = useRef()
-  const password = useRef()
-  const daysLimit = useRef()
-  const downloadLimit = useRef()
-  const [link, setLink] = useState()
-  const [upPercent, setUpPercent] = useState(-1)
-  const isUploading = upPercent >= 0
-  const isUploaded = link && link !== 'error'
-  const maxDaysLimit = type === 'premium' ? 365 : type === 'normal' ? 30 : 3
-  const length = files.length
+	const { fetchApp, files, setFiles, uploadFiles, setUploadFiles, type } = useFileContext()
+	const { share } = router.query
+	const fileIdRef = useRef()
+	const password = useRef()
+	const daysLimit = useRef()
+	const downloadLimit = useRef()
+	const [link, setLink] = useState()
+	const [upPercent, setUpPercent] = useState(-1)
+	const isUploading = upPercent >= 0
+	const isUploaded = link && link !== 'error'
+	const maxDaysLimit = type === 'premium' ? 365 : type === 'normal' ? 30 : 3
+	const length = files.length
 
-  const verifyFileId = e => e.target.value = e.target.value.replace(/[^a-zA-Z0-9_-]/g, "")
-  const verifyDownloadLimit = e => e.target.value = Math.abs(e.target.value) || ''
-  const verifyDaysLimit = e => e.target.value = Math.min(Math.abs(e.target.value), maxDaysLimit) || ''
+	const verifyFileId = e => e.target.value = e.target.value.replace(/[^a-zA-Z0-9_-]/g, "")
+	const verifyDownloadLimit = e => e.target.value = Math.abs(e.target.value) || ''
+	const verifyDaysLimit = e => e.target.value = Math.min(Math.abs(e.target.value), maxDaysLimit) || ''
 
-  function updateFile(event) {
-    const { files } = event.target
-    const size = fileDetails(files).totalSize
-    if (!size) {
-      event.target.value = "";
-      return toast.warning('Empty file(s)');
-    }
-    if (size > limit * 1048576) { // size limit
-      toast('Try Peer-to-peer transfer for big files')
-      setFiles(files)
-      return router.push('/p2p?share=true')
-    }
-    setFiles(files)
-  }
+	function updateFile(event) {
+		const { files } = event.target
+		const size = fileDetails(files).totalSize
+		if (!size) {
+			event.target.value = "";
+			return toast.warning('Empty file(s)');
+		}
+		if (size > limit * 1048576) { // size limit
+			toast('Try Peer-to-peer transfer for big files')
+			setFiles(files)
+			return router.push('/p2p?share=true')
+		}
+		setFiles(files)
+	}
 
-  async function handleSubmit(event) {
-    event.preventDefault()
-    setUpPercent(0)
-    if (length === 1) var content = files[0]
-    else {
-      const zip = new JSZip();
-      for (let i = 0; i < length; i++) zip.file(files[i].name, files[i])
-      content = await zip.generateAsync({ type: 'blob', compression: 'STORE' })
-    }
+	function reset() {
+		setLink()
+		setUpPercent(-1)
+	}
 
-    const data = new FormData();
-    data.append('files', content) // (attribute, value), this is the attribute that we will accept in backend as upload.single/array(attribute which contains the files) where upload is a multer function
-    data.append('length', length)
-    const nameList = []
-    for (let i = 0; i < length; i++) nameList.push(files[i].name)
-    if (fileId = fileIdRef.current.value) {
-      if (options.includes(fileId)) {
-        toast.warning(`File Id cannot be ${fileId}`);
-        setUpPercent(-1)
-        return
-      }
-      data.append('fileId', fileId)
-    }
-    if (length > 1) data.append('nameList', nameList)
-    data.append('password', password.current.value)
-    data.append('daysLimit', daysLimit.current.value)
-    data.append('downloadLimit', downloadLimit.current.value)
+	async function handleSubmit(event) {
+		event.preventDefault()
+		setUpPercent(0)
+		if (length === 1) var content = files[0]
+		else {
+			const zip = new JSZip();
+			for (let i = 0; i < length; i++) zip.file(files[i].name, files[i])
+			content = await zip.generateAsync({ type: 'blob', compression: 'STORE' })
+		}
 
-    let { success: verified, token, server, servers } = await fetchApp({ url: 'file/verify', method: 'POST', data: { fileId } })
-    if (!verified) return setUpPercent(-1)
+		const data = new FormData();
+		data.append('files', content) // (attribute, value), this is the attribute that we will accept in backend as upload.single/array(attribute which contains the files) where upload is a multer function
+		data.append('length', length)
+		const nameList = []
+		for (let i = 0; i < length; i++) nameList.push(files[i].name)
+		if (fileId = fileIdRef.current.value) {
+			if (options.includes(fileId)) {
+				toast.warning(`File Id cannot be ${fileId}`);
+				setUpPercent(-1)
+				return
+			}
+			data.append('fileId', fileId)
+		}
+		if (length > 1) data.append('nameList', nameList)
+		data.append('password', password.current.value)
+		data.append('daysLimit', daysLimit.current.value)
+		data.append('downloadLimit', downloadLimit.current.value)
 
-    while (!success) {
-      if (!servers.length) {
-        setLink('error')
-        setUpPercent(-1)
-        return
-      }
-      var { fileId, name, success } = await fetchApp({
-        url: getUploadUrl(server), method: 'POST', data, type: 'multipart/form-data', token,
-        showToast: servers.length === 1 || 'success', options: {
-          onUploadProgress: ({ loaded, total }) => setUpPercent(Math.round(loaded * 100 / total))
-        }
-      })
-      remove(servers, server)
-      server = randomElement(servers)
-    }
-    setLink(fileId)
-    setUploadFiles(uploadFiles.concat({ _id: fileId, name, nameList, downloadCount: 0, createdAt: Date.now(), daysLimit: daysLimit.current.value || maxDaysLimit }))
-  }
+		let { success: verified, token, server, servers } = await fetchApp({ url: 'file/verify', method: 'POST', data: { fileId } })
+		if (!verified) return setUpPercent(-1)
 
-  useEffect(() => {
-    if (!share) setFiles([])
-    navigator.serviceWorker?.addEventListener('message', ({ data: { files } }) => {
-      setFiles(files)
-      if (fileDetails(files).totalSize > limit * 1048576) router.push('/p2p?share=true')
-    })
-  }, [])
+		while (!success) {
+			if (!servers.length) {
+				setLink('error')
+				setUpPercent(-1)
+				return
+			}
+			var { fileId, name, success } = await fetchApp({
+				url: getUploadUrl(server), method: 'POST', data, type: 'multipart/form-data', token,
+				showToast: servers.length === 1 || 'success', options: {
+					onUploadProgress: ({ loaded, total }) => setUpPercent(Math.round(loaded * 100 / total))
+				}
+			})
+			remove(servers, server)
+			server = randomElement(servers)
+		}
+		setLink(fileId)
+		setUploadFiles(uploadFiles.concat({ _id: fileId, name, nameList, downloadCount: 0, createdAt: Date.now(), daysLimit: daysLimit.current.value || maxDaysLimit }))
+	}
 
-  useEffect(() => { isUploaded && window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }) }, [link])
+	useEffect(() => {
+		if (!share) setFiles([])
+		navigator.serviceWorker?.addEventListener('message', ({ data: { files } }) => {
+			setFiles(files)
+			if (fileDetails(files).totalSize > limit * 1048576) router.push('/p2p?share=true')
+		})
+	}, [])
 
-  return <>
-    <Head><title>Upload a file | CloudBreeze</title></Head>
-    <div className='flex flex-col space-y-5 justify-center items-center px-4 pb-5 text-sm sm:text-base'>
-      <form onSubmit={handleSubmit} className="grid grid-cols-[auto_1fr] gap-3 items-center">
-        <label htmlFor="files">File(s):</label>
-        {share && length ? <div>{length > 1 ? `${length} files` : files[0]?.name} selected</div>
-          : <input type="file" id='files' disabled={isUploading} required onChange={updateFile} multiple />}
+	useEffect(() => { isUploaded && window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }) }, [link])
 
-        <label htmlFor="file-id">File Id: </label>
-        <input type="text" id='file-id' ref={fileIdRef} onInput={verifyFileId} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete='off' placeholder='Auto' maxLength={30} />
+	return <>
+		<Head><title>Upload a file | CloudBreeze</title></Head>
+		<div className='flex flex-col space-y-5 justify-center items-center px-4 pb-5 text-sm sm:text-base'>
+			<form onSubmit={handleSubmit} className="grid grid-cols-[auto_1fr] gap-3 items-center">
+				<label htmlFor="files">File(s):</label>
+				{share && length ? <div>{length > 1 ? `${length} files` : files[0]?.name} selected</div>
+					: <input type="file" id='files' disabled={isUploading} required onChange={updateFile} multiple />}
 
-        <label htmlFor="password">Password:</label>
-        <input type="password" id='password' ref={password} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="new-password" placeholder='No protection' />
+				<label htmlFor="file-id">File Id: </label>
+				<input type="text" id='file-id' ref={fileIdRef} onInput={verifyFileId} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete='off' placeholder='Auto' maxLength={30} />
 
-        <label htmlFor="days-limit">Days Limit:</label>
-        <input type="number" id='days-limit' ref={daysLimit} onInput={verifyDaysLimit} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="off" placeholder={`${maxDaysLimit} (max)`} min={1} max={maxDaysLimit} />
+				<label htmlFor="password">Password:</label>
+				<input type="password" id='password' ref={password} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="new-password" placeholder='No protection' />
 
-        <label htmlFor="download-limit">Download Limit:</label>
-        <input type="number" id='download-limit' ref={downloadLimit} onInput={verifyDownloadLimit} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="off" placeholder='No limit' min={1} />
+				<label htmlFor="days-limit">Days Limit:</label>
+				<input type="number" id='days-limit' ref={daysLimit} onInput={verifyDaysLimit} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="off" placeholder={`${maxDaysLimit} (max)`} min={1} max={maxDaysLimit} />
 
-        <button type="submit" disabled={isUploading} className='primary-button'>Upload</button>
-        {isUploaded && <button type="reset" className='col-span-2 py-1 border border-black rounded bg-gray-100 font-medium text-gray-800' onClick={() => setTimeout(() => setUpPercent(-1), 0)}>Reset</button>}
-      </form>
+				<label htmlFor="download-limit">Download Limit:</label>
+				<input type="number" id='download-limit' ref={downloadLimit} onInput={verifyDownloadLimit} disabled={isUploading} className='border rounded px-2 py-0.5 placeholder:text-sm' autoComplete="off" placeholder='No limit' min={1} />
 
-      {!link && (upPercent === 100 ? <Loader className='flex items-center space-x-2' text='Please wait, processing the file(s)...' /> : <BarProgress percent={upPercent} />)}
+				<button type="submit" disabled={isUploading} className='primary-button'>Upload</button>
+				{isUploaded && <button type="reset" className='col-span-2 py-1 border border-black rounded bg-gray-100 font-medium text-gray-800' onClick={reset}>Reset</button>}
+			</form>
 
-      {isUploaded && <div className='pb-16'><Info fileId={link} /></div>}
-    </div>
-  </>
+			{!link && (upPercent === 100 ? <Loader className='flex items-center space-x-2' text='Please wait, processing the file(s)...' /> : <BarProgress percent={upPercent} />)}
+
+			{isUploaded && <div className='pb-16'><Info fileId={link} /></div>}
+		</div>
+	</>
 }
