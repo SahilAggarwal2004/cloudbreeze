f982c551b8d7ba542b9582de918972a0ea3e9ee6
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-06-12 15:32:21 +0530

    commit

--- pages/p2p/[roomId].js
+++ pages/p2p/[roomId].js
@@ -63,6 +63,8 @@ export default function Id({ router }) {
         connection?.removeAllListeners()
         connection?.close()
         connect()
+        setFile()
+        setText()
         setError()
     }
 
