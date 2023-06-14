6d46876bf26fa6b6e0e41dec40a8f6a2f73f4169
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-06-14 16:30:57 +0530

    commit

--- public/share-sw.js
+++ public/share-sw.js
@@ -1,10 +1,13 @@
 self.skipWaiting()
 
+const paths = ['/file/upload', '/p2p']
+
 self.addEventListener('fetch', e => {
     const { request } = e
     const { pathname } = new URL(request.url);
-    if (request.method === 'POST' && pathname === '/share') {
-        e.respondWith(Response.redirect('/file/upload?share=true'))  // important to tackle cannot post url error
+    if (request.method === 'POST') {
+        if (!paths.includes(pathname)) return
+        e.respondWith(Response.redirect(`${pathname}?share = true`))  // important to tackle cannot post url error
         e.waitUntil(async function () {
             const client = await self.clients.get(e.resultingClientId);
             const data = await e.request.formData();
