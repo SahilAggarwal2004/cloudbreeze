11e71bd285b43fcb3c41bf2cdf8ce90064757034
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-10-01 15:43:49 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -4,71 +4,74 @@ const pages = ['/', '/account', '/account/forgot', '/account/history?filter=uplo
 const images = ['/images/account.webp', '/images/cloud.webp', '/images/arrow.png', '/images/p2p.webp']
 const revision = `${Date.now()}`
 
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   cacheStartUrl: false,
   reloadOnOnline: false,
-  importScripts: ['/share-sw.js'],
-  additionalManifestEntries: pages.concat(images).map(url => ({ url, revision })),
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'static-font-assets' }
-    },
-    {
-      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'static-image-assets' }
-    },
-    {
-      urlPattern: /\/_next\/image\?url=.+$/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'next-image' }
-    },
-    {
-      urlPattern: /\.(?:mp3|wav|ogg)$/i,
-      handler: 'CacheFirst',
-      options: {
-        rangeRequests: true,
-        cacheName: 'static-audio-assets'
+  workboxOptions: {
+    importScripts: ['/share-sw.js'],
+    additionalManifestEntries: pages.concat(images).map(url => ({ url, revision })),
+    offlineGoogleAnalytics: true,
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+        handler: 'CacheFirst',
+        options: { cacheName: 'static-font-assets' }
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: 'CacheFirst',
+        options: { cacheName: 'static-image-assets' }
+      },
+      {
+        urlPattern: /\/_next\/image\?url=.+$/i,
+        handler: 'CacheFirst',
+        options: { cacheName: 'next-image' }
+      },
+      {
+        urlPattern: /\.(?:mp3|wav|ogg)$/i,
+        handler: 'CacheFirst',
+        options: {
+          rangeRequests: true,
+          cacheName: 'static-audio-assets'
+        }
+      },
+      {
+        urlPattern: /\.(?:mp4)$/i,
+        handler: 'CacheFirst',
+        options: {
+          rangeRequests: true,
+          cacheName: 'static-video-assets'
+        }
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: 'StaleWhileRevalidate',
+        options: { cacheName: 'js-assets' }
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: 'CacheFirst',
+        options: { cacheName: 'static-style-assets' }
+      },
+      {
+        urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+        handler: 'StaleWhileRevalidate',
+        options: { cacheName: 'next-data' }
+      },
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: 'NetworkFirst',
+        options: { cacheName: 'static-data-assets' }
+      },
+      {
+        urlPattern: () => true,
+        handler: 'NetworkOnly',
+        options: { cacheName: 'others' }
       }
-    },
-    {
-      urlPattern: /\.(?:mp4)$/i,
-      handler: 'CacheFirst',
-      options: {
-        rangeRequests: true,
-        cacheName: 'static-video-assets'
-      }
-    },
-    {
-      urlPattern: /\.(?:js)$/i,
-      handler: 'StaleWhileRevalidate',
-      options: { cacheName: 'js-assets' }
-    },
-    {
-      urlPattern: /\.(?:css|less)$/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'static-style-assets' }
-    },
-    {
-      urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-      handler: 'StaleWhileRevalidate',
-      options: { cacheName: 'next-data' }
-    },
-    {
-      urlPattern: /\.(?:json|xml|csv)$/i,
-      handler: 'NetworkFirst',
-      options: { cacheName: 'static-data-assets' }
-    },
-    {
-      urlPattern: () => true,
-      handler: 'NetworkOnly',
-      options: { cacheName: 'others' }
-    }
-  ]
+    ]
+  }
 })
 
 const nextConfig = {
