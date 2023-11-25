010daf022d3c82814ac00d879ff8e19038cdd6d0
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-11-25 22:29:31 +0530

    commit

--- pages/account/confirm/[token].js
+++ pages/account/confirm/[token].js
@@ -11,6 +11,7 @@ export default function Confirm({ router }) {
 
   return <>
     <Head><title>Confirm account | CloudBreeze</title></Head>
+    <h1 className='sticky inset-0 z-30 bg-black text-white py-2 px-5 shadow-lg text-xl font-medium text-center sm:text-left'>CloudBreeze</h1>
     <div className='center space-y-5 text-center'>
       <h3 className='text-lg font-semibold'>Confirm your CloudBreeze account</h3>
       <button className='mt-1 py-1 px-2 rounded-md border-[1.5px] border-black text-white bg-black hover:text-black hover:bg-white transition-all duration-300' onClick={verify}>Click Here!</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     screens: {
