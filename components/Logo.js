46f3379721a50614f76bfb17effdf985b3fead5d
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-11-26 23:01:29 +0530

    commit

--- components/Logo.js
+++ components/Logo.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 export default function Logo() {
     return <div className="h-16 text-center select-none">
-        <img src="/logo.png" alt="CloudBreeze" width={75} fetchPriority='high' className='mx-auto' />
+        <img src="/logo.png" alt="CloudBreeze" width={75} className='mx-auto' />
     </div>
 }
--- pages/index.js
+++ pages/index.js
@@ -16,21 +16,21 @@ export default function Home({ router }) {
 
     return <div className='flex flex-col mx-[5vw] mb-12 space-y-12 md:flex-row md:space-x-6 md:space-y-0 items-center md:justify-center lg:justify-evenly text-center md:h-[calc(100vh-8rem)]'>
         <div className='image-container pt-12 pb-24' onClick={() => redirect('/account')} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/account.webp' alt='' width={176} fetchPriority='high' />
+            <img src='/images/account.webp' alt='' width={176} />
             <div className='x-center bottom-4'>Your account</div>
         </div>
         <div className='image-container pt-1 pb-16' onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} onClick={() => {
             setHover(true);
             redirect('/file/upload')
         }} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/cloud.webp' alt='' width={250} fetchPriority='high' />
+            <img src='/images/cloud.webp' alt='' width={250} />
             <div className={`x-center top-[55%] md:top-44 transition-all duration-300 ${hover ? '-translate-y-[5.5rem] opacity-100' : 'opacity-0'}`}>
                 <img src='/images/arrow.png' alt='' width={50} fetchPriority='low' />
             </div>
             <div className='x-center bottom-4'>Upload a file</div>
         </div>
         <div className='image-container pt-12 pb-24' onClick={() => redirect('/p2p')} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/p2p.webp' alt='' width={176} fetchPriority='high' />
+            <img src='/images/p2p.webp' alt='' width={176} />
             <div className='x-center bottom-4'>Peer-to-peer transfer</div>
         </div>
     </div>
