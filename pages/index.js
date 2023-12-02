8c86ca9058136985ca71ae22c9d0f27a38554f00
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-02 20:36:00 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -14,23 +14,27 @@ export default function Home({ router }) {
         }, 300)
     }
 
-    return <div className='flex flex-col mx-[5vw] mb-12 space-y-12 md:flex-row md:space-x-6 md:space-y-0 items-center md:justify-center lg:justify-evenly text-center md:h-[calc(100vh-8rem)]'>
-        <div className='image-container pt-1 pb-16' onMouseEnter={() => setHover('upload')} onMouseLeave={() => setHover()} onClick={() => redirect('/file/upload')} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/upload.webp' alt='' width={250} />
-            <div className={`x-center top-44 transition-all duration-300 ${hover === 'upload' ? '-translate-y-[5.5rem]' : 'opacity-0'}`}>
-                <img src='/images/arrow.png' alt='' width={50} fetchPriority='low' />
+    return <div className='flex flex-col sm:flex-wrap mx-[5vw] mb-10 sm:flex-row gap-x-8 gap-y-10 items-center sm:justify-center text-center sm:h-[calc(100vh-11rem)]'>
+        <div className='image-container' onClick={() => redirect('/account')} onContextMenu={e => e.preventDefault()}>
+            <img src='/images/account.webp' alt='' className='min-h-full aspect-square scale-90' />
+            <div className='x-center bottom-4'>Your account</div>
+        </div>
+        <div className='image-container items-end' onMouseEnter={() => setHover('upload')} onMouseLeave={() => setHover()} onClick={() => redirect('/file/upload')} onContextMenu={e => e.preventDefault()}>
+            <img src='/images/upload.webp' alt='' className='min-h-full aspect-square' />
+            <div className={`x-center top-[57%] transition-all duration-300 ${hover === 'upload' ? '-translate-y-[5rem]' : 'opacity-0'}`}>
+                <img src='/images/arrow.png' alt='' className='scale-95' fetchPriority='low' />
             </div>
             <div className='x-center bottom-4'>Upload files</div>
         </div>
-        <div className='image-container pt-1 pb-16' onMouseEnter={() => setHover('download')} onMouseLeave={() => setHover()} onClick={() => redirect('/file/download')} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/download.webp' alt='' width={250} className='opacity-95' />
-            <div className={`x-center top-3 transition-all duration-300 rotate-180 ${hover === 'download' ? 'translate-y-[3.5rem] opacity-100' : 'opacity-0'}`}>
-                <img src='/images/arrow.png' alt='' width={40} fetchPriority='low' />
+        <div className='image-container' onMouseEnter={() => setHover('download')} onMouseLeave={() => setHover()} onClick={() => redirect('/file/download')} onContextMenu={e => e.preventDefault()}>
+            <img src='/images/download.webp' alt='' className='min-h-full aspect-square scale-90 top-0 opacity-95' />
+            <div className={`x-center top-0 transition-all duration-300 rotate-180 ${hover === 'download' ? 'top-[20%]' : 'opacity-0'}`}>
+                <img src='/images/arrow.png' alt='' className='scale-[0.6]' fetchPriority='low' />
             </div>
             <div className='x-center bottom-4'>Download files</div>
         </div>
-        <div className='image-container pt-12 pb-24' onClick={() => redirect('/p2p')} onContextMenu={e => e.preventDefault()}>
-            <img src='/images/p2p.webp' alt='' width={176} />
+        <div className='image-container' onClick={() => redirect('/p2p')} onContextMenu={e => e.preventDefault()}>
+            <img src='/images/p2p.webp' alt='' className='min-h-full aspect-square scale-90' />
             <div className='x-center bottom-4'>Peer-to-peer transfer</div>
         </div>
     </div>
