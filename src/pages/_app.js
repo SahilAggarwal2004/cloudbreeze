77538080c230ae43d48bcbc6476848886eee1b95
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-07 17:40:55 +0530

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,7 +24,7 @@ export default function MyApp({ Component, pageProps }) {
         <Head>
             <meta charSet="utf-8" />
             <title>CloudBreeze - Breeze your files on the cloud!</title>
-            <link rel="icon" href="./favicon.ico" />
+            <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#ffffff" />
             <meta name="keywords"
