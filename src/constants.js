8714e5ec0aac2391ef030748d9062f6ba3e3853a
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-01-14 13:20:46 +0530

    added more turn servers

--- src/constants.js
+++ src/constants.js
@@ -17,23 +17,26 @@ export const maxConnections = 12
 export const minChunkSize = 128 * sizes.KB
 
 // P2P
+const accounts = [
+    { username: "23d2d924e3c7a3d878d3ecb1", credential: "PYCjdq025mRAMqu5" },
+    { username: "26757539465fdba06ea1880c", credential: "vMyhFZr6d6Wflths" },
+    { username: "4634db834cc4cd836dd62801", credential: "+Gp9d9TcMmHl1P0Y" }
+]
 export const peerOptions = {
     host: 'cloudbreeze-peer.onrender.com', secure: true, pingInterval: 5000,
     config: {
         iceServers: [
             { urls: "stun:stun.l.google.com:19302" },
-            { urls: "stun:stun.relay.metered.ca:80" },
-            {
-                urls: [
-                    "turn:standard.relay.metered.ca:80",
-                    "turn:standard.relay.metered.ca:80?transport=tcp",
-                    "turn:standard.relay.metered.ca:443",
-                    "turns:standard.relay.metered.ca:443?transport=tcp"
-                ],
-                username: process.env.NEXT_PUBLIC_TURN_SERVER_USERNAME,
-                credential: process.env.NEXT_PUBLIC_TURN_SERVER_PASSWORD
-            }
-        ]
+            { urls: "stun:stun.relay.metered.ca:80" }
+        ].concat(accounts.map(account => ({
+            urls: [
+                "turn:standard.relay.metered.ca:80",
+                "turn:standard.relay.metered.ca:80?transport=tcp",
+                "turn:standard.relay.metered.ca:443",
+                "turns:standard.relay.metered.ca:443?transport=tcp"
+            ],
+            ...account
+        })))
     }
 }
 export const chunkSize = 64 * sizes.KB;
