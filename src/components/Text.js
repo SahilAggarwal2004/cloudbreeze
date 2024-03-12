1b7793cbd26284f8c4cabea47676d1fa5dc6a9bc
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-03-12 16:05:56 +0530

    commit

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,7 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useLayoutEffect, useState } from "react";
 import { FaCopy } from "react-icons/fa";
-import Speech, { HighlightedText } from "react-text-to-speech";
+import { HiVolumeOff, HiVolumeUp } from "react-icons/hi";
+import { useSpeech } from "react-text-to-speech";
 import { Balancer } from "react-wrap-balancer";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -10,21 +11,24 @@ import { toast } from "react-toastify";
 
 export default function Text({ id, text }) {
   const [markdown, setMarkdown] = useState("");
-
-  const mdText = (
-    <div className="markdown">
-      {markdown ? (
-        parse(markdown)
-      ) : (
-        <Markdown className={`markdown-${id}`} remarkPlugins={[remarkGfm]}>
-          {text}
-        </Markdown>
-      )}
-    </div>
-  );
+  const { Text, speechStatus, start, stop } = useSpeech({
+    text: (
+      <div className="markdown">
+        {markdown ? (
+          parse(markdown)
+        ) : (
+          <Markdown className={`markdown-${id}`} remarkPlugins={[remarkGfm]}>
+            {text}
+          </Markdown>
+        )}
+      </div>
+    ),
+    highlightText: true,
+  });
 
   useLayoutEffect(() => {
-    setMarkdown("");
+    stop();
+    setTimeout(() => setMarkdown(""), 1);
   }, [text]);
 
   useLayoutEffect(() => {
@@ -37,17 +41,19 @@ export default function Text({ id, text }) {
   }
 
   return (
-    <div className="flex flex-col items-center px-4 text-justify space-y-2.5">
+    <div className="flex flex-col items-center px-4 sm:px-5 md:px-6 text-justify space-y-2.5">
       <div className="flex justify-around w-[80vw] max-w-60">
         <span className="text-lg font-medium">Text</span>
         <div className="flex items-center space-x-2">
-          <FaCopy className="cursor-pointer" onClick={copy} />
-          <Speech id={id} text={mdText} useStopOverPause highlightText />
+          <button>
+            <FaCopy onClick={copy} />
+          </button>
+          <button className="scale-125">{speechStatus === "started" ? <HiVolumeOff onClick={stop} /> : <HiVolumeUp onClick={start} />}</button>
         </div>
       </div>
       <div className="whitespace-pre-line max-w-full overflow-x-scroll" style={{ wordBreak: "break-word" }}>
         <Balancer>
-          <HighlightedText id={id}>{mdText}</HighlightedText>
+          <Text />
         </Balancer>
       </div>
     </div>
